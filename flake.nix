2291b72d0b59d3e5b64ebb71529b550a87605e43
Mihai Fufezan <mihai@fufexan.net>
2024-12-24 14:46:26 +0200

    flake: warn about unavailable generic and specific packages

--- flake.nix
+++ flake.nix
@@ -9,13 +9,21 @@
     self,
     nixpkgs,
   }: let
+    inherit (nixpkgs) lib;
+
+    inherit (lib.trivial) warn;
+    inherit (lib.attrsets) genAttrs;
+    inherit (builtins) fromJSON readFile;
+
     systems = ["aarch64-linux" "x86_64-linux"];
-    eachSystem = nixpkgs.lib.genAttrs systems;
+    eachSystem = genAttrs systems;
     pkgs = nixpkgs.legacyPackages;
 
-    info = builtins.fromJSON (builtins.readFile ./info.json);
+    info = fromJSON (readFile ./info.json);
 
     mkZen = system: sourceInfo: pkgs.${system}.callPackage ./package.nix {inherit sourceInfo;};
+
+    warningMessage = variant: "zen-browser-flake: the `${variant}` package is no longer available. Use the `zen` package instead.";
   in {
     packages = eachSystem (system: {
       zen = mkZen system {
@@ -23,6 +31,8 @@
         inherit (info) version;
       };
       default = self.packages.${system}.zen;
+      generic = warn (warningMessage "generic") self.packages.${system}.zen;
+      specific = warn (warningMessage "specific") self.packages.${system}.zen;
     });
   };
 }
